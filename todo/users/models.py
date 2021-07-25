04be15b8d2f07b1e19a3431de34432bca9567fb1
enesGune <enes_1244_@hotmail.com>
2021-07-25 21:46:31 +0300

    logout

--- todo/users/models.py
+++ todo/users/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class LoginUser(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
--- todo/users/views.py
+++ todo/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from .forms import LoginForm
-from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login, logout, get_user_model
 # Create your views here.
+
+User = get_user_model()
 
 
 def login_user(request, *args, **kwargs):
@@ -40,3 +42,30 @@ def login_view(request):
             "form": form,
         }
     )
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/login")
+
+
+def register_view(request):
+    form = RegisterForm(request.POST or None)
+
+    if form.is_valid():
+
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password1")
+        password2 = form.cleaned_data.get("password2")
+        try:
+            user = User.objects.create_user(username, email, password)
+        except:
+            user = None
+        if user != None:
+            login(request, user)
+            return redirect("/")
+        else:
+            request.session['register_error'] = 1
+
+    return render(request, "login.html", {"form": form})
